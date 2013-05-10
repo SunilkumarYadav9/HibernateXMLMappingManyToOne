9805ed9afa819359ed87318d04dc9eb6315542f1
cavalr <blog@cavalr.com>
2013-05-09 23:11:25 -0500

    Added to Git

---
+++ src/main/java/com/blog/cavalr/hibernate/Resume.java
@@ -0,0 +1,34 @@
+package com.blog.cavalr.hibernate;
+
+public class Resume {
+
+    private Long resumeId;
+
+    private String name;
+
+    private Address address;
+
+    public Long getResumeId() {
+        return resumeId;
+    }
+
+    public void setResumeId(Long resumeId) {
+        this.resumeId = resumeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
